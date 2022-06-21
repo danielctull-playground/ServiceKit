43a8fc1e55f9d9f9409ed347d623c75c98d0f1e2
Daniel Tull <dt@danieltull.co.uk>
2022-06-21 16:40:22 +0100

    Add initial Services struct

--- Sources/ServiceKit/ServiceKit.swift
+++ Sources/ServiceKit/ServiceKit.swift
@@ -1,3 +1,17 @@
 
-struct ServiceKit {
+public protocol ServiceKey {
+    associatedtype Value
+    static var defaultValue: Value { get }
+}
+
+public struct Services {
+
+    public init() {}
+
+    private var values: [ObjectIdentifier: Any] = [:]
+
+    public subscript<Key: ServiceKey>(key: Key.Type) -> Key.Value {
+        get { values[ObjectIdentifier(key)] as? Key.Value ?? Key.defaultValue }
+        set { values[ObjectIdentifier(key)] = newValue }
+    }
 }
--- Tests/ServiceKitTests/ServiceKitTests.swift
+++ Tests/ServiceKitTests/ServiceKitTests.swift
@@ -2,5 +2,20 @@
 import ServiceKit
 import XCTest
 
+struct TestKey: ServiceKey {
+    static let defaultValue = "Hello"
+}
+
 final class ServiceKitTests: XCTestCase {
+
+    func testGetType() {
+        let services = Services()
+        XCTAssertEqual(services[TestKey.self], "Hello")
+    }
+
+    func testSetType() {
+        var services = Services()
+        services[TestKey.self] = "World"
+        XCTAssertEqual(services[TestKey.self], "World")
+    }
 }
